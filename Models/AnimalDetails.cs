f6976c3031f92203cc09cfe122df20ef02a3954c
Tony555x <antonkremov2007@gmail.com>
2025-10-24 11:23:48 +0300

    Refactor AnimalDetails

--- Models/AnimalDetails.cs
+++ Models/AnimalDetails.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using code_refactoring.Data.Models;
 
 namespace Namespace
 {
@@ -13,6 +14,17 @@ namespace Namespace
         public int Age{ get; set; } = 0;
         public string Type{ get; set; } = String.Empty;
         public bool Sick{ get; set; } = false;
-        public string Notes{ get; set; } = String.Empty;
+        public string Notes { get; set; } = String.Empty;
+        public AnimalDetails FromAnimal(Animal a)
+        {
+            Id = a.Id;
+            Name = a.Name;
+            Owner = a.Owner;
+            Age = a.Age;
+            Type = a.Type;
+            Sick = a.Sick;
+            Notes = a.Notes;
+            return this;
+        }   
     }
 }
